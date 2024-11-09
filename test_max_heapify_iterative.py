276c5eedf2501973e1291b56e886c4fe5e093e1a
tarikuamisganaw <tarikmisgana@gmail.com>
2024-11-09 17:03:16 +0300

    Update test_max_heapify_iterative.py

--- test_max_heapify_iterative.py
+++ test_max_heapify_iterative.py
@@ -5,7 +5,7 @@ def test_max_heapify_sorted_array():
     arr = [1, 2, 3, 4, 5]
     max_heapify(arr, len(arr), 0)
     # After max_heapify, the array should reflect max-heap property
-    assert arr == [5, 4, 3, 2, 1]  # Expected output after max_heapify
+    assert arr ==[3, 2, 1, 4, 5]  # Expected output after max_heapify
 
 def test_max_heapify_reverse_sorted_array():
     arr = [5, 4, 3, 2, 1]
@@ -17,7 +17,7 @@ def test_max_heapify_unsorted_array():
     arr = [3, 5, 1, 10, 2, 7]
     max_heapify(arr, len(arr), 0)
     # After max_heapify, the largest element should be at the root
-    assert arr == [10, 5, 1, 3, 2, 7]  # Expected output after max_heapify
+    assert arr == [5, 10, 1, 3, 2, 7]  # Expected output after max_heapify
 
 def test_max_heapify_empty_array():
     arr = []
